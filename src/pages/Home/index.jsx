7b79a83e628c8e59e41ed2e41234939b114736ce
Vu Nguyen <vucules@gmail.cpm>
2022-06-17 13:32:47 +0700

    2

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,27 @@
+import { useEffect,useState } from "react";
+import axios from "axios";
 export default function Index() {
+  const [result, setResult] = useState([]);
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setResult(response.data);
+    }
+    fetch();
+  },[]);
   return (
     <div>
-      <h1>Home</h1>
+      {
+        !result && <div>Loading...</div>
+      }
+      <ul>
+        {result.map(e => {
+          return <li key={e.id}>
+            {e.id} - {e.title} <br />
+            {e.body}
+          </li>
+        })}
+      </ul>
     </div>
   );
 }
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,5 +1,3 @@
-import { lazy } from "react";
-
 export default [{
     name: 'ReportKPI',
     path: '/report-kpi',
